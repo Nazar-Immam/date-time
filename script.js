7e52e2ec9d50db908fefa9a9be49a000662a050a
Nazar-Immam <156925750+Nazar-Immam@users.noreply.github.com>
2025-03-09 01:46:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("hi I am alert")  to check if the page is linked or not
+
+const timeElement = document.getElementById("time-element") ;
+const dateElement = document.getElementById("date-element") ;
+
+const now = new Date() ;
+
+
+const hours = now.getHours() % 12 || 12 ;  // this becomes a condition so the true will get executed, and 0 is treated as false so 12 
+const mins = now.getMinutes().toString().padStart(2,"0") ; // this assures the string has atleast 2 value if not then it will fill it with "0"
+const seconds = now.getSeconds()
+const second = seconds<10 ? `0${seconds}`: `${seconds}`;
+const ampm = hours < 12 ? "AM" : "PM" ;
+
+timeElement.textContent = `${hours}:${mins}: ${se}`
+
