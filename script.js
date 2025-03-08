cf32bcb0901c794c2097aaeaef6e5fa04c0afd05
Nazar-Immam <156925750+Nazar-Immam@users.noreply.github.com>
2025-03-09 01:49:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // alert("hi I am alert")  to check if the page is linked or not
 
-const timeElement = document.getElementById("time-element") ;
-const dateElement = document.getElementById("date-element") ;
+function updateclock(){
+    const timeElement = document.getElementById("time-element") ;
+    const dateElement = document.getElementById("date-element") ;
 
-const now = new Date() ;
+    const now = new Date() ;
 
 
-const hours = now.getHours() % 12 || 12 ;  // this becomes a condition so the true will get executed, and 0 is treated as false so 12 
-const mins = now.getMinutes().toString().padStart(2,"0") ; // this assures the string has atleast 2 value if not then it will fill it with "0"
-const seconds = now.getSeconds()
-const second = seconds<10 ? `0${seconds}`: `${seconds}`;
-const ampm = hours < 12 ? "AM" : "PM" ;
+    const hrs = now.getHours() % 12 || 12 ;  // this becomes a condition so the true will get executed, and 0 is treated as false so 12
+    const hours = hrs < 10 ? `0${hrs}` : `${hrs}`; 
+    const mins = now.getMinutes().toString().padStart(2,"0") ; // this assures the string has atleast 2 value if not then it will fill it with "0"
+    const seconds = now.getSeconds()
+    const second = seconds<10 ? `0${seconds}`: `${seconds}`;
+    const ampm = hours < 12 ? "AM" : "PM" ;
 
-timeElement.textContent = `${hours}:${mins}: ${se}`
+    timeElement.textContent = `${hours}:${mins}:${second}:${ampm}` ;
 
+    dateElement.textContent =  now.toDateString() ;
+
+}
+updateclock()
+
+setInterval(updateclock, 1000)  
